827132414c725a9372d8197106b85daa3ef1444a
18561564228 <Q1w2e3r4>
2025-12-25 00:02:56 +0800

    demo1添加方法

--- git-demo/src/main/java/com/git/controller/DemoController.java
+++ git-demo/src/main/java/com/git/controller/DemoController.java
@@ -5,4 +5,13 @@ public class DemoController {
     public void demo() {
         System.out.println("Hello World111");
     }
+
+    public void demo1() {
+        System.out.println("Hello World111");
+    }
+
+
+    public void demo11() {
+        System.out.println("Hello World111");
+    }
 }
