616af420232c60468f8736e5140589f5c5530865
18561564228 <Q1w2e3r4>
2025-12-24 23:56:11 +0800

    demo1、demo2修改

--- git-demo/src/main/java/com/git/controller/Demo2Controller.java
+++ git-demo/src/main/java/com/git/controller/Demo2Controller.java
@@ -1,4 +1,8 @@
 package com.git.controller;
 
 public class Demo2Controller {
+
+    public void demo2() {
+        System.out.println("Hello2");
+    }
 }
--- git-demo/src/main/java/com/git/controller/DemoController.java
+++ git-demo/src/main/java/com/git/controller/DemoController.java
@@ -3,6 +3,6 @@ package com.git.controller;
 public class DemoController {
 
     public void demo() {
-        System.out.println("Hello World");
+        System.out.println("Hello World111");
     }
 }
